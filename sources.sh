4a3e001b359bec5465a3f178fe6f0655f19b1c21
Eugene Yokota <eed3si9n@gmail.com>
2019-12-26 18:50:43 -0500

    publish IO using sbt

--- sources.sh
+++ sources.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-export BUILD_VERSION="1.3.0-bin-${TIMESTAMP}"
+export BUILD_VERSION="1.4.0-bin-${TIMESTAMP}"
 echo "The build version is: ${BUILD_VERSION}"
 cd proj/io
-sbt -Dsbt.build.version=${BUILD_VERSION} -sbt-dir $WORKSPACE/.sbt -ivy $WORKSPACE/.ivy -no-colors publishLocal
+sbt -Dsbt.sbtbintray=false -Dsbt.build.localmaven=${LOCAL_MAVEN} -Dsbt.build.version=${BUILD_VERSION} -sbt-dir $WORKSPACE/.sbt -ivy $WORKSPACE/.ivy -no-colors publishLocal publish
 cd ../..
