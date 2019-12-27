947b0e8abd52e92a1013338bc9a8b331cc64b482
Eugene Yokota <eed3si9n@gmail.com>
2019-12-27 02:24:48 -0500

    utils

--- sources.sh
+++ sources.sh
@@ -4,4 +4,5 @@ export BUILD_VERSION="1.4.0-bin-${TIMESTAMP}"
 echo "The build version is: ${BUILD_VERSION}"
 cd proj/io
 sbt -Dsbt.sbtbintray=false -Dsbt.build.localmaven=${LOCAL_MAVEN} -Dsbt.build.version=${BUILD_VERSION} -sbt-dir $WORKSPACE/.sbt -ivy $WORKSPACE/.ivy -no-colors publishLocal publish
-cd ../..
+cd ../../proj/sbt/
+sbt -Dsbt.sbtbintray=false -Dsbt.build.localmaven=${LOCAL_MAVEN} -Dsbt.build.version=${BUILD_VERSION} -sbt-dir $WORKSPACE/.sbt -ivy $WORKSPACE/.ivy -no-colors clean lowerUtils/publishLocal lowerUtils/publish
